483b67b11b63176035f78c08d7e11ced2b445300
Abnegazar <57896757+Abnegazar@users.noreply.github.com>
2019-11-21 09:37:30 +0100

    Fin partielle des API du client

--- src/main/java/com/ecole/ecommerce/services/ClientService.java
+++ src/main/java/com/ecole/ecommerce/services/ClientService.java
@@ -2,6 +2,7 @@ package com.ecole.ecommerce.services;
 
 import com.ecole.ecommerce.domaine.Client;
 import com.ecole.ecommerce.repository.ClientRepository;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,42 +17,86 @@ public class ClientService {
         this.clientRepository = clientRepository;
     }
 
+    /**
+     * Ajouter un nouveau client
+     * @param cli
+     * @return
+     */
     public Client save(Client cli){
        return clientRepository.save(cli);
     }
 
+    /**
+     * Ajouter plusieurs clients è la fois
+     * @param clients
+     * @return
+     */
     public List<Client> saveAll(List<Client> clients){
         return clientRepository.saveAll(clients);
     }
 
+    /**
+     * Récupérer les informations d'un client
+     * @param id
+     * @return un ou zéro(dans le cas ou le client n'existe pas) objets de type Client
+     */
     public Optional<Client> getOne(Long id){
         return clientRepository.findById(id);
     }
 
+    /**
+     * Récupérer les informations de tous les clients
+     * @return
+     */
     public List<Client> getAll(){
         return clientRepository.findAll();
     }
 
+    /**
+     * Met à jour les informations d'un client particulier
+     * @param cli
+     * @return
+     */
     public Client update(Client cli){
         return clientRepository.saveAndFlush(cli);
     }
 
+    /**
+     * Supprime un client donné de la base de données
+     * @param id
+     */
     public void deleteOne(Long id){
         clientRepository.deleteById(id);
     }
 
+    /**
+     * Supprime les informations de tous les clients présent dans la base
+     */
     public void deleteAll(){
         clientRepository.deleteAll();
     }
 
+    /***
+     * Supprime les informations de plusieurs clients à la fois
+     * @param clients
+     */
     public void deleteMany(List<Client> clients){
         clientRepository.deleteAll(clients);
     }
 
+    /**
+     * Compte le nombre de clients présent dans la base
+     * @return
+     */
     public long count(){
         return clientRepository.count();
     }
 
+    /**
+     * Vérifie la présence ou non d'un client dans la base
+     * @param id
+     * @return
+     */
     public boolean existsById(Long id){
         return clientRepository.existsById(id);
     }
--- src/main/java/com/ecole/ecommerce/web/ClientResources.java
+++ src/main/java/com/ecole/ecommerce/web/ClientResources.java
@@ -2,11 +2,17 @@ package com.ecole.ecommerce.web;
 
 import com.ecole.ecommerce.domaine.Client;
 import com.ecole.ecommerce.services.ClientService;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ClientResources {
@@ -17,10 +23,53 @@ public class ClientResources {
         this.clientService = clientService;
     }
 
-    @PostMapping(value = "/clients")
+    @PostMapping(value = "/client")
     public ResponseEntity<Client> save(@RequestBody Client cli){
-        return new ResponseEntity<Client>(clientService.save(cli), HttpStatus.CREATED);
+        return new ResponseEntity<>(clientService.save(cli), HttpStatus.CREATED);
     }
 
+    /////////////////////////////
+    ///////  A revoir  /////////
+    ////////////////////////////
+    @PostMapping(value = "/clients")
+    public ResponseEntity<List<Client>> saveAll(List<Client> clients){
+        return new ResponseEntity<>(clientService.saveAll(clients), HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/client/{id}")
+    public ResponseEntity<Optional<Client>> getOne(@PathVariable("id") Long id){
+        return new ResponseEntity<>(clientService.getOne(id), HttpStatus.FOUND);
+    }
+
+    @GetMapping(value = "/clients")
+    public ResponseEntity<List<Client>> getAll(){
+        return new ResponseEntity<>(clientService.getAll(), HttpStatus.OK);
+    }
+
+    /////////////////////////////
+    ///////  A revoir  /////////
+    ////////////////////////////
+    @PutMapping("/client")
+    public ResponseEntity<Client> update(Client cli){
+        return new ResponseEntity<>(clientService.update(cli), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/client/{id}")
+    public void deleteOne(@PathVariable Long id){
+        clientService.deleteOne(id);
+    }
+
+    @DeleteMapping("/client")
+    public void deleteAll(){
+        clientService.deleteAll();
+    }
+
+    /////////////////////////////
+    ///////  A revoir  /////////
+    ////////////////////////////
+    @DeleteMapping("/clients")
+    public void deleteMany(List<Client> clients){
+        clientService.deleteMany(clients);
+    }
 
 }
